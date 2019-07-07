7ad72073f4c04267886446912c81150b206ef78e
Princy Mishra <princymishra1997.5@gmail.com>
2019-07-07 14:05:58 +0530

    Using String Builder

---
+++ StringManipulation.java
@@ -0,0 +1,16 @@
+package assessment;
+
+public class StringManipulation {
+
+	public static void main(String[] args) {
+			StringBuilder builder = new StringBuilder();
+			String first = "Prol";
+			String second = "ifics";
+			builder.append(first);
+			builder.append(second);
+			builder.append("\tHYDERABAD");
+	System.out.println(builder);		
+	}
+}
+
+
